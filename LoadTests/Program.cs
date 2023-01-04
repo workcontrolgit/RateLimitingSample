995a5ecbc140b82c10f69048a67d766e84c62ff7
Fuji <fuji.nguyen@workcontrol.com>
2023-01-04 13:09:38 -0500

    Add LoadTests using NBomber

---
+++ LoadTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using NBomber.Configuration;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+
+var client = ScenariousHelper.CreateClient();
+var scenario = ScenariousHelper.GetToDoTestScenario(client);
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .WithReportFormats(ReportFormat.Html, ReportFormat.Md)
+    .Run();
+
+Console.WriteLine("Press any key ...");
+Console.ReadKey();
---
+++ LoadTests/ScenariousHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+
+    public static class ScenariousHelper
+    {
+        public static Scenario GetToDoTestScenario(HttpClient httpClient)
+        {
+            var callDateNowStep = Step.Create("call todos/v2", async (context) =>
+            {
+                var response = await httpClient.GetAsync("todos/v2");
+                if (response.IsSuccessStatusCode)
+                    return Response.Ok(statusCode: (int)response.StatusCode);
+                else
+                    return Response.Fail(statusCode: (int)response.StatusCode);
+            });
+
+            var scenario = ScenarioBuilder.CreateScenario("call todos/v2", callDateNowStep)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+                .WithLoadSimulations(
+                    LoadSimulation.NewInjectPerSec(_rate: 100, _during: TimeSpan.FromSeconds(30))
+                );
+
+            return scenario;
+        }
+
+    public static HttpClient CreateClient()
+    {
+
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var baseUrl = configuration.GetValue<string>("BaseUrl");
+        if (string.IsNullOrEmpty(baseUrl))
+            throw new ArgumentException("Cannot read configuration");
+
+        var httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri(baseUrl);
+        return httpClient;
+    }
+}
